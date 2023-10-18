7b5750135b2e674bca968de176972f8d433a47a3
bhroben <116118720+bhroben@users.noreply.github.com>
2023-10-18 21:51:07 +0200

    constrain file problem
    synthesis ok, implementation failed due to constrain file error.
    TODO: understand it and redo it

--- demux/nbit_demux.vhd
+++ demux/nbit_demux.vhd
@@ -34,8 +34,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity nbit_demux is
     generic (n: NATURAL :=3);
     port (D: in std_logic_vector(n-1 downto 0); -- S0 and S1 are in S
-          S0: in std_logic;
-          S1: in std_logic;
+          S: std_logic_vector(1 downto 0);
           Y0 : out std_logic_vector(n-1 downto 0);
           Y1 : out std_logic_vector(n-1 downto 0);
           Y2 : out std_logic_vector(n-1 downto 0);
@@ -44,13 +43,12 @@ end nbit_demux;
 
 architecture diego of nbit_demux is
 
-    
-begin
-    
-    Y0 <= not (S0&S1) and D;
-    Y1 <= not (S0)&S1 and D;
-    Y2 <=  S0& not(S1) and D;
-    Y3 <= S0&S1 and D;
 
+begin
+
+    Y0 <= D when S="00" else ( others => '0');
+    Y1 <= D when S="01" else ( others => '0');
+    Y2 <=  D when S="10" else ( others => '0');
+    Y3 <= D when S="11" else ( others => '0');
 
 end diego;
